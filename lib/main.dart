beebc67c6811dc27802d9a1fad600582d3e371c5
Gafouruser <yekiniabdougafour@gmail.com>
2024-11-01 13:55:19 +0100

    all edits

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:device_preview/device_preview.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:projet_mobile/data/screens/animated.dart';
 
@@ -15,6 +14,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
         locale: DevicePreview.locale(context),
